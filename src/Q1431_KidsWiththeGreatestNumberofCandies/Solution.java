728250db90737fa65f2e61f7d274dc94c74ad25f
Xixiang-Liu <xixiangliuoscar@gmail.com>
2024-10-20 10:09:39 -0500

    finish 1431

---
+++ src/Q1431_KidsWiththeGreatestNumberofCandies/Solution.java
@@ -0,0 +1,16 @@
+package Q1431_KidsWiththeGreatestNumberofCandies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        for (int candy : candies)
+            maxCandies = Math.max(maxCandies, candy);
+        List<Boolean> res = new ArrayList<>();
+        for (int candy : candies)
+            res.add(candy + extraCandies >= maxCandies);
+        return res;
+    }
+}
