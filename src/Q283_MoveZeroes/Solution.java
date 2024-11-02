5b4d3a1282516d45f0c6e7ffefd786775255dc19
Xixiang-Liu <xixiangliuoscar@gmail.com>
2024-11-01 19:48:50 -0500

    finish 283

---
+++ src/Q283_MoveZeroes/Solution.java
@@ -0,0 +1,15 @@
+package Q283_MoveZeroes;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int p = 0;  // write pointer
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[p++] = nums[i];
+            }
+        }
+        for (int i = p; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
